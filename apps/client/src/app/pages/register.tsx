c5e62ffed59a508348838df154b52905fd62aba6
Lean Junio <leanjunio@outlook.com>
2022-10-06 12:49:07 -0400

    style: update button hover text

--- apps/client/src/app/pages/register.tsx
+++ apps/client/src/app/pages/register.tsx
@@ -81,7 +81,7 @@ export function Register() {
               <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                 <button
                   type="submit"
-                  className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                  className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
                 >
                   Create an account
                 </button>
