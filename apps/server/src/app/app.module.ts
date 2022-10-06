fd2adce322c0e11a005569047af1da91ae9d9a00
Lean Junio <leanjunio@outlook.com>
2022-10-06 13:39:48 -0400

    infra: import mongoose

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongo/courtside-db')],
   controllers: [AppController],
   providers: [AppService],
 })
