08c0aea254dac52c560e3659a74c4f00f2b50f5f
Lean Junio <leanjunio@outlook.com>
2022-10-07 11:41:50 -0400

    fix: imports

--- libs/data-access/src/lib/data-access.spec.ts
+++
@@ -1,7 +0,0 @@
-import { dataAccess } from './data-access';
-
-describe('dataAccess', () => {
-  it('should work', () => {
-    expect(dataAccess()).toEqual('data-access');
-  });
-});
--- libs/data-access/src/lib/data-access.ts
+++
@@ -1,3 +0,0 @@
-export function dataAccess(): string {
-  return 'data-access';
-}
