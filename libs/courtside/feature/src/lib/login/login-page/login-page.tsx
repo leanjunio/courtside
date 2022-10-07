cb014eaa9fae9ff195e3f0a8b5eebd58921a7cd5
Lean Junio <leanjunio@outlook.com>
2022-10-07 18:26:53 -0400

    feat: add login page

--- libs/courtside/feature/src/lib/login/login-page/login-page.tsx
+++ libs/courtside/feature/src/lib/login/login-page/login-page.tsx
@@ -5,23 +5,21 @@ export function Login() {
   return (
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+        <aside className="relative block h-16 lg:order-first lg:col-span-5 lg:h-full xl:col-span-6">
           <img
             alt="sideline"
-            src="https://images.unsplash.com/photo-1599677104038-58d80f6976e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80"
+            src="https://images.unsplash.com/photo-1563506644863-444710df1e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
           />
         </aside>
 
         <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
+          <div className="w-full">
             <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Signup
+              Login
             </h1>
 
-            <p className="mt-4 leading-relaxed text-gray-500">
-              Please start by entering the following details. The following
-              information would be used for your communication with your
-              teammates
+            <p className="w-max mt-4 leading-relaxed text-gray-500">
+              Enter your login information
             </p>
             <LoginForm />
           </div>
