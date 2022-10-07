0382f594c485fac52ab4b8fbb9107f6b02799d1b
Lean Junio <leanjunio@outlook.com>
2022-10-07 17:06:40 -0400

    refactor: move signup form to lib

---
+++ libs/courtisde/feature/src/lib/signup/signup-form/signup-form.tsx
@@ -0,0 +1,67 @@
+import { CreateUserDto } from '@courtside/data-access';
+import { EmailField, PasswordField, TextField } from '@courtside/ui/fields';
+import { useForm } from 'react-hook-form';
+
+export function SignupForm() {
+  const { reset, handleSubmit, register } = useForm<CreateUserDto>();
+
+  const onSubmit = (data: CreateUserDto) => {
+    reset();
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-8 grid grid-cols-6 gap-6"
+    >
+      <TextField
+        htmlFor="FirstName"
+        label="First Name"
+        {...register('firstName')}
+      />
+      <TextField
+        htmlFor="LastName"
+        label="Last Name"
+        {...register('lastName')}
+      />
+
+      <div className="col-span-6">
+        <EmailField htmlFor="Email" label="Email" {...register('email')} />
+      </div>
+
+      <div className="col-span-6 sm:col-span-3">
+        <PasswordField
+          htmlFor="Password"
+          label="Password"
+          {...register('password')}
+        />
+      </div>
+
+      <div className="col-span-6 sm:col-span-3">
+        <PasswordField
+          htmlFor="PasswordConfirmation"
+          label="Confirm Password"
+          {...register('passwordConfirmation')}
+        />
+      </div>
+
+      <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+        <button
+          type="submit"
+          className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
+        >
+          Create an account
+        </button>
+
+        <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+          Already have an account?{' '}
+          <a href="/login" className="text-gray-700 underline">
+            Log in
+          </a>
+          .
+        </p>
+      </div>
+    </form>
+  );
+}
