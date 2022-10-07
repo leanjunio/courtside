82030220848f3e10fbd1b39ea4c3b174c13687f2
Lean Junio <leanjunio@outlook.com>
2022-10-07 11:30:54 -0400

    refactor: remove unused app components

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
-import { router } from './app/router';
+import { router } from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
