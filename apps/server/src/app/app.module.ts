aed0b5b79b3a9b20128f2239ea22f1b3c4eaeb47
Lean Junio <leanjunio@outlook.com>
2022-10-06 20:36:11 -0400

    feat: add nest modules

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo/courtside-db')],
+  imports: [MongooseModule.forRoot('mongodb://mongo/courtside-db'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
