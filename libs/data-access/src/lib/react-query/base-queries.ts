a377138ebc872f8c2a16a69a2454bc70f84a9199
Lean Junio <leanjunio@outlook.com>
2022-10-08 15:35:53 -0400

    feat: create base queries

---
+++ libs/data-access/src/lib/react-query/base-queries.ts
@@ -0,0 +1,40 @@
+import {
+  QueryKey,
+  useMutation,
+  UseMutationResult,
+  useQuery,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { BaseRequests } from '../axios';
+
+export type BaseQueries<Entity> = {
+  useCreateOne: () => UseMutationResult<Entity, AxiosError, Entity>;
+  useUpdateOne: () => UseMutationResult<Entity, AxiosError, Entity>;
+  useReplaceOne: () => UseMutationResult<Entity, AxiosError, Entity>;
+  useGetOne: (id: string) => UseQueryResult<Entity, AxiosError>;
+  useGetAll: () => UseQueryResult<Entity[], AxiosError>;
+};
+
+export function createBaseQueries<Entity>(
+  queryKey: QueryKey,
+  requests: BaseRequests<Entity>
+): BaseQueries<Entity> {
+  return {
+    useCreateOne: () => {
+      return useMutation((data) => requests.createOne(data));
+    },
+    useUpdateOne: () => {
+      return useMutation((data) => requests.updateOne(data));
+    },
+    useReplaceOne: () => {
+      return useMutation((data) => requests.replaceOne(data));
+    },
+    useGetOne: (id: string) => {
+      return useQuery([queryKey, id], () => requests.getOne(id));
+    },
+    useGetAll: () => {
+      return useQuery([queryKey], () => requests.getAll);
+    },
+  };
+}
