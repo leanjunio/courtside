4ed3fe251f42279e8dfe987ad18fef7fcb16779c
Lean Junio <leanjunio@outlook.com>
2022-10-08 15:34:55 -0400

    feat: create base axios requests

--- libs/data-access/src/lib/axios/request.ts
+++ libs/data-access/src/lib/axios/request.ts
@@ -1,18 +1,50 @@
 import { getEnvironmentVariables } from '@courtside/shared/util-environment';
 import axios from 'axios';
 
-export function createBaseAxiosRequests<R, Data = unknown>(url: string) {
+type AxiosRequests<Response, Data = unknown> = {
+  getAll: (url?: string) => Promise<Response[]>;
+  get: (url?: string) => Promise<Response>;
+  post: (data: Data) => Promise<Response>;
+  put: (data: Data) => Promise<Response>;
+  patch: (data: Data) => Promise<Response>;
+  delete: (url?: string) => Promise<Response>;
+};
+
+export function createBaseAxiosRequests<R, Data = unknown>(
+  url: string
+): AxiosRequests<R, Data> {
   const axiosInstance = axios.create({
     baseURL: getEnvironmentVariables('NX_API_SERVER'),
+    url,
   });
 
-  const requests = {
-    get: () => axiosInstance.get<R>(url),
-    post: (data?: Data) => axiosInstance.post<R>(url, data),
-    put: (data?: Data) => axiosInstance.put<R>(url, data),
-    patch: (data?: Data) => axiosInstance.patch<R>(url, data),
-    delete: () => axiosInstance.get<R>(url),
+  return {
+    getAll: (overwrittenUrl: string = url) => {
+      return axiosInstance({ method: 'get', url: overwrittenUrl });
+    },
+    get: (overwrittenUrl: string = url) => {
+      return axiosInstance({ method: 'get', url: overwrittenUrl });
+    },
+    post: (data) => {
+      return axiosInstance({ method: 'post', data });
+    },
+    put: (data) => {
+      return axiosInstance({ method: 'put', data });
+    },
+    patch: (data) => {
+      return axiosInstance({ method: 'patch', data });
+    },
+    delete: (overwrittenUrl: string = url) => {
+      return axiosInstance({ method: 'delete', url: overwrittenUrl });
+    },
   };
-
-  return requests;
 }
+
+export type BaseRequests<Entity> = {
+  createOne: (data: Entity) => Promise<Entity>;
+  updateOne: (data: Entity) => Promise<Entity>;
+  replaceOne: (data: Entity) => Promise<Entity>;
+  getOne: (id: string) => Promise<Entity>;
+  getAll: () => Promise<Entity[]>;
+  deleteOne: (id: string) => Promise<Entity>;
+};
