f007c0e302eb4183aeb0badbd69ad88263f5a31d
Lean Junio <leanjunio@outlook.com>
2022-10-08 16:22:29 -0400

    fix: remove unused modules

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,13 +1,8 @@
+import { UserModule } from '@courtside/entities';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from '../user/user.module';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://mongo/courtside-db'), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
