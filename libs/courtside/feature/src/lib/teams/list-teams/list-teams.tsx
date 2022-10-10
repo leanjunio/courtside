d574396e5a3d844e4704979ed4451db88edb5a67
Lean Junio <leanjunio@outlook.com>
2022-10-10 18:10:09 -0400

    feat: expand list teams

--- libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
+++ libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
@@ -1,9 +1,14 @@
-import { PageWrapper } from '@courtside/ui';
+import { CircleButton, PageWrapper } from '@courtside/ui';
 
 export function ListTeams() {
   return (
-    <PageWrapper>
-      <h1>Teams</h1>
+    <PageWrapper title="Teams">
+      <div className="flex justify-center items-center h-full">hello</div>
+      <div className="relative">
+        <div className="absolute bottom-0 right-0 m-10">
+          <CircleButton label="Create Team" />
+        </div>
+      </div>
     </PageWrapper>
   );
 }
