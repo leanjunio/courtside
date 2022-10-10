ff676eb97718c983b467a2d85b97c435cf72c79b
Lean Junio <leanjunio@outlook.com>
2022-10-09 20:10:51 -0400

    feat: use auth0provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,3 +1,5 @@
+import { Auth0Provider } from '@auth0/auth0-react';
+import { getEnvironmentVariables } from '@courtside/shared/util-environment';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
@@ -12,8 +14,13 @@ const queryClient = new QueryClient();
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Auth0Provider
+      clientId={getEnvironmentVariables('NX_AUTH0_CLIENT_ID')}
+      domain={getEnvironmentVariables('NX_AUTH0_DOMAIN')}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Auth0Provider>
   </StrictMode>
 );
