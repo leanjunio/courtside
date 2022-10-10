7687017f22ee5b45964c34c3c9f81664c24f81e9
Lean Junio <leanjunio@outlook.com>
2022-10-10 11:58:35 -0400

    feat: add create team header

--- libs/courtside/feature/src/lib/teams/create-team/create-team.tsx
+++ libs/courtside/feature/src/lib/teams/create-team/create-team.tsx
@@ -1,5 +1,9 @@
 import { PageWrapper } from '@courtside/ui';
 
 export function CreateTeam() {
-  return <PageWrapper>Create a Team</PageWrapper>;
+  return (
+    <PageWrapper>
+      <p className="text-2xl font-bold">Create a Team</p>
+    </PageWrapper>
+  );
 }
