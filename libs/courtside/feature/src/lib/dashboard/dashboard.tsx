6b4660fe7eed659d7091c0cc532d56136d323c0a
Lean Junio <leanjunio@outlook.com>
2022-10-10 09:22:06 -0400

    refactor: moved auth0 to page wrapper

--- libs/courtside/feature/src/lib/dashboard/dashboard.tsx
+++ libs/courtside/feature/src/lib/dashboard/dashboard.tsx
@@ -1,14 +1,6 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Sidebar } from '@courtside/ui';
+import { PageWrapper } from '@courtside/ui';
 import React from 'react';
 
 export function Dashboard() {
-  const { isAuthenticated, user } = useAuth0();
-
-  return (
-    <div className="flex">
-      {isAuthenticated && user !== undefined && <Sidebar user={user} />}
-      My Content
-    </div>
-  );
+  return <PageWrapper>Content</PageWrapper>;
 }
