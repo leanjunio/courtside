b220c6092e6f70365e59b757a350a40d800a7f90
Lean Junio <leanjunio@outlook.com>
2022-10-11 12:58:52 -0400

    chore: use base requests

--- libs/data-access/src/lib/teams/requests.ts
+++ libs/data-access/src/lib/teams/requests.ts
@@ -1,22 +1,4 @@
 import { Team } from '@courtside/entities';
-import { createBaseAxiosRequests } from '../axios';
+import { createBaseRequests } from '../axios';
 
-const baseTeamRequests = createBaseAxiosRequests<Team, Team>('/teams');
-
-export type TeamRequests = {
-  createOne: (data: Team) => Promise<Team>;
-  updateOne: (data: Team) => Promise<Team>;
-  replaceOne: (data: Team) => Promise<Team>;
-  getOne: (id: string) => Promise<Team>;
-  getAll: () => Promise<Team[]>;
-  deleteOne: (id: string) => Promise<Team>;
-};
-
-export const teamRequests: TeamRequests = {
-  createOne: (data: Team) => baseTeamRequests.post(data),
-  updateOne: (data: Team) => baseTeamRequests.put(data),
-  replaceOne: (data: Team) => baseTeamRequests.patch(data),
-  getOne: (id: string) => baseTeamRequests.get(`/user/${id}`),
-  getAll: () => baseTeamRequests.getAll(`/user`),
-  deleteOne: (id: string) => baseTeamRequests.delete(`/user/${id}`),
-};
+export const baseTeamRequests = createBaseRequests<Team, Team>('/teams');
--- libs/data-access/src/lib/users/requests.ts
+++ libs/data-access/src/lib/users/requests.ts
@@ -1,22 +1,4 @@
 import { User } from '@courtside/entities';
-import { createBaseAxiosRequests } from '../axios';
+import { createBaseRequests } from '../axios';
 
-const baseUserRequests = createBaseAxiosRequests<User, User>('/user');
-
-export type UserRequests = {
-  createOne: (data: User) => Promise<User>;
-  updateOne: (data: User) => Promise<User>;
-  replaceOne: (data: User) => Promise<User>;
-  getOne: (id: string) => Promise<User>;
-  getAll: () => Promise<User[]>;
-  deleteOne: (id: string) => Promise<User>;
-};
-
-export const userRequests: UserRequests = {
-  createOne: (data: User) => baseUserRequests.post(data),
-  updateOne: (data: User) => baseUserRequests.put(data),
-  replaceOne: (data: User) => baseUserRequests.patch(data),
-  getOne: (id: string) => baseUserRequests.get(`/user/${id}`),
-  getAll: () => baseUserRequests.getAll(`/user`),
-  deleteOne: (id: string) => baseUserRequests.delete(`/user/${id}`),
-};
+export const baseUserRequests = createBaseRequests<User, User>('/users');
