59243f201fa5b5b18d34edfac3da6953b1c2d3a8
Lean Junio <leanjunio@outlook.com>
2022-10-11 19:33:44 -0400

    feat: use direct axios post

--- libs/data-access/src/lib/auth/requests.ts
+++ libs/data-access/src/lib/auth/requests.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 import { LoginUserDto } from '../users';
 
 const baseAuthUrl = getEnvironmentVariables('NX_API_SERVER') + '/auth';
+
 export function login(data: LoginUserDto) {
-  return axios({ url: `${baseAuthUrl}/login`, method: 'post', data }).then(
-    (res) => res.data
-  );
+  return axios.post(`${baseAuthUrl}/login`, data).then((res) => res.data);
 }
