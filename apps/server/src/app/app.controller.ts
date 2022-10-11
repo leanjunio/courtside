fe89cee3efed238170c9352db04a38faff42b71d
Lean Junio <leanjunio@outlook.com>
2022-10-11 18:35:51 -0400

    feat: use imported services for login and basic get

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,11 +1,19 @@
-import { LocalAuthGuard } from '@courtside/entities';
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService, JwtAuthGuard, LocalAuthGuard } from '@courtside/entities';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
