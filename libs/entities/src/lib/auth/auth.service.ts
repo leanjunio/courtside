4f1e519305f6ee5a5ffb7cc4fceb924f821a8f37
Lean Junio <leanjunio@outlook.com>
2022-10-11 19:03:16 -0400

    feat: change key of payload

--- libs/entities/src/lib/auth/auth.service.ts
+++ libs/entities/src/lib/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
 
   async login(user: User) {
     return {
-      access_token: this.jwtService.sign({ email: user.email }),
+      access_token: this.jwtService.sign({ sub: user.email }),
     };
   }
 }
