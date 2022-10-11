04ffb0c914f20040c3b244399e7f665796a46e52
Lean Junio <leanjunio@outlook.com>
2022-10-11 15:42:30 -0400

    feat: export local auth guard

--- libs/entities/src/lib/auth/auth.module.ts
+++ libs/entities/src/lib/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
