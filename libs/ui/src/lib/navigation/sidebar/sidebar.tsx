d94fabbb637f1903913b466667179dd13c6a0769
Lean Junio <leanjunio@outlook.com>
2022-10-11 14:44:56 -0400

    feat: create details link

--- libs/ui/src/lib/navigation/sidebar/sidebar.tsx
+++ libs/ui/src/lib/navigation/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import { useAuth0, User } from '@auth0/auth0-react';
-import { MdOutlineAssignment } from 'react-icons/md';
+import { MdOutlineAssignment, MdOutlineAssignmentInd } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 export type SidebarProps = {
@@ -218,27 +218,13 @@ export function Sidebar({ user }: SidebarProps) {
             </summary>
 
             <nav className="mt-1.5 ml-8 flex flex-col">
-              <a
-                href="#"
-                className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              <Link
+                to="/details"
+                className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 opacity-75"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
-                  />
-                </svg>
-
-                <span className="ml-3 text-sm font-medium"> Details </span>
-              </a>
+                <MdOutlineAssignmentInd className="h-5 w-5" />
+                <span className="ml-3 text-sm font-medium">Details</span>
+              </Link>
 
               <a
                 href="#"
