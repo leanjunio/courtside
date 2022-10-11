f229d4bce720f7240de6869cbb0fae2a58ce7402
Lean Junio <leanjunio@outlook.com>
2022-10-11 15:42:12 -0400

    feat: use PassportModule in auth module

--- libs/entities/src/lib/auth/auth.module.ts
+++ libs/entities/src/lib/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   providers: [AuthService, LocalStrategy, LocalAuthGuard],
 })
 export class AuthModule {}
