bbe7011b70116dc62a880252f7a6782c9f622fd6
Lean Junio <leanjunio@outlook.com>
2022-10-11 13:05:08 -0400

    feat: create route for finding all teams

--- libs/entities/src/lib/team/team.controller.ts
+++ libs/entities/src/lib/team/team.controller.ts
@@ -1,11 +1,16 @@
 import { MongoExceptionFilter } from '@courtside/shared/util-server-exceptions';
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { CreateTeamDto } from './team';
 import { TeamService } from './team.service';
 
 @Controller('Team')
 export class TeamController {
   constructor(private teamService: TeamService) {}
+  @Get()
+  findAll() {
+    return this.teamService.findAll();
+  }
+
   @Post()
   @UseFilters(MongoExceptionFilter)
   create(@Body() createTeamDto: CreateTeamDto) {
