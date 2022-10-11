57ea31ee6fb9811e074c39b8e66313ab7db7e34e
Lean Junio <leanjunio@outlook.com>
2022-10-11 15:42:38 -0400

    feat: use local auth guard in app

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import { LocalAuthGuard } from '@courtside/entities';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
