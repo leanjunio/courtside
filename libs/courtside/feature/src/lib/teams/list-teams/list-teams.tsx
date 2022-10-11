08f5b498af9864f5daf175a9a18ae66d17adc5b7
Lean Junio <leanjunio@outlook.com>
2022-10-11 14:17:28 -0400

    fix: add initial output

--- libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
+++ libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
@@ -1,16 +1,37 @@
 import { baseTeamQueries } from '@courtside/data-access';
-import { CircleButton, EmptyState, PageWrapper } from '@courtside/ui';
+import {
+  CircleButton,
+  EmptyState,
+  LoadingSpinner,
+  PageWrapper,
+} from '@courtside/ui';
+import { useSnackbar } from 'notistack';
 
 export function ListTeams() {
-  const { data } = baseTeamQueries.useGetAll();
+  const { status, data } = baseTeamQueries.useGetAll();
+  const { enqueueSnackbar } = useSnackbar();
 
-  console.log({ data });
+  if (status === 'error') {
+    enqueueSnackbar('Could not fetch teams');
+  }
+
+  let output: JSX.Element = <></>;
+
+  if (status === 'loading') {
+    output = <LoadingSpinner />;
+  }
+
+  if (status === 'success') {
+    if (data.length === 0) {
+      output = <EmptyState entityName="Team" />;
+    } else {
+      data.map((d) => <p>{d.name}</p>);
+    }
+  }
 
   return (
     <PageWrapper title="Teams">
-      <div className="flex justify-center items-center h-full">
-        <EmptyState entityName="Team" />
-      </div>
+      <div className="flex justify-center items-center h-full">{output}</div>
       <div className="relative">
         <div className="absolute bottom-0 right-0 m-10">
           <CircleButton label="Create Team" />
