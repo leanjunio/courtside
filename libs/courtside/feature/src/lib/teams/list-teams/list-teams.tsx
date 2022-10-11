7248c181d6d31dca3efc5ff433f491b7106f8c47
Lean Junio <leanjunio@outlook.com>
2022-10-11 14:01:08 -0400

    feat: use empty state

--- libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
+++ libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
@@ -1,9 +1,16 @@
-import { CircleButton, PageWrapper } from '@courtside/ui';
+import { baseTeamQueries } from '@courtside/data-access';
+import { CircleButton, EmptyState, PageWrapper } from '@courtside/ui';
 
 export function ListTeams() {
+  const { data } = baseTeamQueries.useGetAll();
+
+  console.log({ data });
+
   return (
     <PageWrapper title="Teams">
-      <div className="flex justify-center items-center h-full">hello</div>
+      <div className="flex justify-center items-center h-full">
+        <EmptyState entityName="Team" />
+      </div>
       <div className="relative">
         <div className="absolute bottom-0 right-0 m-10">
           <CircleButton label="Create Team" />
