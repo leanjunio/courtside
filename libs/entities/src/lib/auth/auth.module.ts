7456055924477716906dab656aa07c71b7f138d3
Lean Junio <leanjunio@outlook.com>
2022-10-11 19:24:42 -0400

    feat: increase jwt expiry to 20 mins

--- libs/entities/src/lib/auth/auth.module.ts
+++ libs/entities/src/lib/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: getEnvironmentVariables('JWT_SECRET'),
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '20m' },
     }),
   ],
   providers: [AuthService, LocalStrategy, LocalAuthGuard, JwtStrategy],
