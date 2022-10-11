e2a90dc88740f1e6dc76dd41e91c9484abd55294
Lean Junio <leanjunio@outlook.com>
2022-10-11 15:40:46 -0400

    feat: create local passport strategy
    This will be used to validate whether a user exists or not

--- libs/entities/src/lib/auth/auth.module.ts
+++ libs/entities/src/lib/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [UserModule],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, LocalAuthGuard],
 })
 export class AuthModule {}
