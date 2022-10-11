c6e61d4df667f487cb5736c4db9e34f81cb26830
Lean Junio <leanjunio@outlook.com>
2022-10-11 18:35:38 -0400

    feat: define login for jwt

--- libs/entities/src/lib/auth/auth.service.ts
+++ libs/entities/src/lib/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User, UserService } from '../user';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.verifyAuthentication(email, password);
@@ -13,5 +17,11 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  async login(user: User) {
+    return {
+      access_token: this.jwtService.sign({ email: user.email }),
+    };
   }
 }
