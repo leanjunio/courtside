18bac066a057003121fe8cda0ee960c56511abda
Lean Junio <leanjunio@outlook.com>
2022-10-14 07:53:45 -0400

    feat: use util

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { AuthModule, TeamModule, UserModule } from '@courtside/entities';
+import { getEnvironmentVariables } from '@courtside/shared/util-environment';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      pass: process.env.DB_PASSWORD,
+    MongooseModule.forRoot(getEnvironmentVariables('DB_URI'), {
+      dbName: getEnvironmentVariables('DB_NAME'),
+      user: getEnvironmentVariables('DB_USERNAME'),
+      pass: getEnvironmentVariables('DB_PASSWORD'),
     }),
     UserModule,
     TeamModule,
