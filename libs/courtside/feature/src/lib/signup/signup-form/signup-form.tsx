2c21cae14d856c326adb5d0d953e8b234f8f3028
Lean Junio <leanjunio@outlook.com>
2022-10-15 12:13:07 -0400

    fix: move reset to button click
    This makes it so that the form resets whether or not the
    handleSubmit is triggered

--- libs/courtside/feature/src/lib/signup/signup-form/signup-form.tsx
+++ libs/courtside/feature/src/lib/signup/signup-form/signup-form.tsx
@@ -15,7 +15,6 @@ export function SignupForm() {
   const signup = useSignup();
 
   const onSubmit = (data: CreateUserDto) => {
-    reset();
     signup.mutate(data, {
       onSuccess: () => {
         navigate('/login');
@@ -54,6 +53,7 @@ export function SignupForm() {
 
       <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
         <button
+          onClick={() => reset()}
           type="submit"
           className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
         >
