c3b27c8e8d505267e464ef91c198db072bbb71fb
Lean Junio <leanjunio@outlook.com>
2022-10-15 10:24:59 -0400

    feat: add error to email field

--- libs/ui/fields/src/lib/email/email-field.tsx
+++ libs/ui/fields/src/lib/email/email-field.tsx
@@ -15,18 +15,23 @@ export function EmailField<TFieldValues extends FieldValues>({
     <Controller
       name={name}
       control={control}
-      render={({ field }) => (
-        <div className="col-span-6 sm:col-span-3">
-          <label className="block text-sm font-medium text-gray-700">
-            {label}
-          </label>
+      render={({ field, formState: { errors } }) => {
+        const error = errors[name]?.message?.toString();
 
-          <input
-            className="mt-1 p-3 border w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-            {...field}
-          />
-        </div>
-      )}
+        return (
+          <div className="col-span-6 sm:col-span-3">
+            <label className="block text-sm font-medium text-gray-700">
+              {label}
+            </label>
+
+            <input
+              className="mt-1 p-3 border w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+              {...field}
+            />
+            {error && <span className="text-xs p-2 text-red-500">{error}</span>}
+          </div>
+        );
+      }}
     />
   );
 }
