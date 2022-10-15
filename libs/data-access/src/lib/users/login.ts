4559675ce63e8efcfad4284f9d213b5c142cda96
Lean Junio <leanjunio@outlook.com>
2022-10-15 13:37:56 -0400

    improvement: add errors to login schema

--- libs/data-access/src/lib/users/login.ts
+++ libs/data-access/src/lib/users/login.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const LoginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z
+    .string({ required_error: 'Required' })
+    .min(1, 'Required')
+    .email('Please enter a valid email'),
+  password: z.string({ required_error: 'Required' }).min(1, 'Required'),
 });
 
 export type LoginUserDto = z.infer<typeof LoginUserSchema>;
