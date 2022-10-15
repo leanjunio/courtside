50a277612496c21b6234b6b8476396f5a331a4ab
Lean Junio <leanjunio@outlook.com>
2022-10-15 13:37:46 -0400

    improvement: add errors to signup schema

--- libs/data-access/src/lib/users/signup.ts
+++ libs/data-access/src/lib/users/signup.ts
@@ -1,11 +1,16 @@
 import { z } from 'zod';
 
-export const signupSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-  passwordConfirmation: z.string(),
-});
+export const signupSchema = z
+  .object({
+    firstName: z.string().min(1, 'Required'),
+    lastName: z.string().min(1, 'Required'),
+    email: z.string().min(1, 'Required').email('Please enter a valid email'),
+    password: z.string().min(1, 'Required'),
+    passwordConfirmation: z.string().min(1, 'Required'),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: `Passwords don't match`,
+    path: ['confirm'],
+  });
 
 export type SignupDto = z.infer<typeof signupSchema>;
