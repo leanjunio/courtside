8e81192c01460c7797189182779213291fa4194c
Lean Junio <leanjunio@outlook.com>
2022-10-15 13:46:25 -0400

    fix: remove reset from submit click

--- libs/courtside/feature/src/lib/signup/signup-form/signup-form.tsx
+++ libs/courtside/feature/src/lib/signup/signup-form/signup-form.tsx
@@ -61,7 +61,6 @@ export function SignupForm() {
 
       <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
         <button
-          onClick={() => reset()}
           type="submit"
           className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
         >
