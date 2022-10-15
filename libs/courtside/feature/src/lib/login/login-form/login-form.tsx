8cdfd3213b1886d255a304af8cceeed546643055
Lean Junio <leanjunio@outlook.com>
2022-10-14 21:28:13 -0400

    feat: use devtools and use email field

--- libs/courtside/feature/src/lib/login/login-form/login-form.tsx
+++ libs/courtside/feature/src/lib/login/login-form/login-form.tsx
@@ -4,7 +4,8 @@ import {
   LoginUserSchema,
   useLogin,
 } from '@courtside/data-access';
-import { PasswordField, TextField } from '@courtside/ui/fields';
+import { EmailField, PasswordField, TextField } from '@courtside/ui/fields';
+import { DevTool } from '@hookform/devtools';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useSnackbar } from 'notistack';
 import { useForm } from 'react-hook-form';
@@ -16,6 +17,7 @@ export function LoginForm() {
     handleSubmit,
     register,
     formState: { errors },
+    control,
   } = useForm<LoginUserDto>({
     resolver: zodResolver(LoginUserSchema),
   });
@@ -53,36 +55,39 @@ export function LoginForm() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit, (errors) => {
-        console.log({ errors });
-      })}
-      className="mt-8 grid grid-cols-6 gap-6"
-    >
-      <TextField htmlFor="Email" label="Email" {...register('email')} />
-      <PasswordField
-        htmlFor="Password"
-        label="Password"
-        {...register('password')}
-      />
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit, (errors) => {
+          console.log({ errors });
+        })}
+        className="mt-8 grid grid-cols-6 gap-6"
+      >
+        <EmailField htmlFor="Email" label="Email" {...register('email')} />
+        <PasswordField
+          htmlFor="Password"
+          label="Password"
+          {...register('password')}
+        />
 
-      <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
-        <button
-          type="submit"
-          className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
-        >
-          Login
-        </button>
-        <p className="mt-4 text-sm sm:mt-0">
-          <a
-            role="button"
-            onClick={onCancel}
-            className="text-gray-500 underline"
+        <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+          <button
+            type="submit"
+            className="inline-block shrink-0 rounded-md border border-amber-600 bg-amber-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-amber-600 focus:outline-none focus:ring active:text-amber-500"
           >
-            Cancel
-          </a>
-        </p>
-      </div>
-    </form>
+            Login
+          </button>
+          <p className="mt-4 text-sm sm:mt-0">
+            <a
+              role="button"
+              onClick={onCancel}
+              className="text-gray-500 underline"
+            >
+              Cancel
+            </a>
+          </p>
+        </div>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 }
