090d06c36960fac38ac308d86f3593ea4932a840
Lean Junio <leanjunio@outlook.com>
2022-10-15 16:54:21 -0400

    fix: remove composite

---
+++ libs/courtside/feature/src/lib/teams/create-team/create-team-modal.tsx
@@ -0,0 +1,18 @@
+import { BasicModal } from '@courtside/ui';
+import { useState } from 'react';
+import { CreateTeamForm } from './create-team-form';
+
+export function CreateTeamModal() {
+  const [modal, setModal] = useState(false);
+
+  return (
+    <BasicModal
+      isOpen={modal}
+      setOpen={setModal}
+      description="Enter your team's details"
+      title="Create a Team"
+    >
+      <CreateTeamForm onCancel={() => console.log('cancel')} />
+    </BasicModal>
+  );
+}
