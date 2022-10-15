70eba731f37393a45b77a94bc1e6a114e8cd7cd7
Lean Junio <leanjunio@outlook.com>
2022-10-15 16:24:14 -0400

    feat: add modal

--- libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
+++ libs/courtside/feature/src/lib/teams/list-teams/list-teams.tsx
@@ -1,15 +1,18 @@
 import { baseTeamQueries } from '@courtside/data-access';
 import {
+  BasicModal,
   CircleButton,
   EmptyState,
   LoadingSpinner,
   PageWrapper,
 } from '@courtside/ui';
 import { useSnackbar } from 'notistack';
+import { useState } from 'react';
 
 export function ListTeams() {
   const { status, data } = baseTeamQueries.useGetAll();
   const { enqueueSnackbar } = useSnackbar();
+  const [modal, setModal] = useState(false);
 
   if (status === 'error') {
     enqueueSnackbar('Could not fetch teams');
@@ -31,10 +34,19 @@ export function ListTeams() {
 
   return (
     <PageWrapper title="Teams">
-      <div className="flex justify-center items-center h-full">{output}</div>
+      <div className="flex justify-center items-center h-full">
+        <BasicModal
+          isOpen={modal}
+          setOpen={setModal}
+          description="this is a description of the modal"
+          message="test"
+          title="test"
+        />
+        {output}
+      </div>
       <div className="relative">
         <div className="absolute bottom-0 right-0 m-10">
-          <CircleButton label="Create Team" />
+          <CircleButton label="Create Team" onClick={() => setModal(true)} />
         </div>
       </div>
     </PageWrapper>
