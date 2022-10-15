ff7b47937e322cb463824cbaab41179672e04866
Lean Junio <leanjunio@outlook.com>
2022-10-15 14:29:20 -0400

    chore: remove unused

--- libs/courtside/navigation/src/lib/courtside-navigation.spec.ts
+++
@@ -1,7 +0,0 @@
-import { courtsideNavigation } from './courtside-navigation';
-
-describe('courtsideNavigation', () => {
-  it('should work', () => {
-    expect(courtsideNavigation()).toEqual('courtside-navigation');
-  });
-});
--- libs/courtside/navigation/src/lib/courtside-navigation.ts
+++
@@ -1,3 +0,0 @@
-export function courtsideNavigation(): string {
-  return 'courtside-navigation';
-}
